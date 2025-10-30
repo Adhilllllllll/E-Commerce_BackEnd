e356a2c7a9e0222e4d3ee50809dc8d313ca0e0b9
adhil <adhilp387@gmail.com>
2025-10-30 20:30:15 +0530

    completd admin side

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,9 +7,13 @@ async function authenticateUser(req, res, next) {
   try {
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if (!token) throw new Error("Please login first");
+    console.log(token);
+    
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const user = await User.findById(decoded.id);
+    console.log(decoded);
+    
+    const user = await User.findById(decoded.Id);
 
     if (!user) throw new Error("User not found. Please login again");
 
